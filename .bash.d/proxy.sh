e8598b06d92cdd9f960bb59648c7d7d974b272be
stahnma <stahnma@websages.com>
2009-12-30 14:14:54 -0600

    Trying something that won't resolv externally for DNS.

--- .bash.d/proxy.sh
+++ .bash.d/proxy.sh
@@ -1,5 +1,5 @@
 
-if (host beaker.cis.cat.com &> /dev/null ) ; then
+if (host ncc01905.fpd.cat.com &> /dev/null ) ; then
   export http_proxy=http://stahnma:${WEB_PIN}@webproxy.fpd.cat.com
   export no_proxy=cat.com
   export HTTP_PROXY=http://stahnma:${WEB_PIN}@webproxy.fpd.cat.com
