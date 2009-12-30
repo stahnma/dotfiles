139b55f5c6d614b36764d152b71b706e2e2c4cd2
stahnma <stahnma@websages.com>
2009-12-30 13:48:09 -0600

    Adding git setup file.

---
+++ .bash.d/git-config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git config --global user.name "stahnma"
+git config --global user.email "stahnma@websages.com"
+git config --global color.diff "auto"
+git config --global color.status "auto"
+git config --global color.branch "auto"
+git config --global color.ui "auto"
+git config --global github.user "stahnma"
+git config --global github.token "$GITHUB_TOKEN"
+
