5d9a72d8c096d89c09fb00dfa3c34b9a8351f073
stahnma <stahnma@websages.com>
2010-03-30 22:13:19 -0500

    Adding good DNS settings for Mac OS clients.

--- .bash.d/01_resolver.sh
+++ .bash.d/01_resolver.sh
@@ -1,3 +1,8 @@
 # Resolver stuff
 
 [ -f /usr/local/sbin/resolver.sh ] && sudo /usr/local/sbin/resolver.sh
+
+if [ `uname` == "Darwin" ] ; then
+   networksetup -setsearchdomains AirPort stahnkage.com websages.com
+   networksetup -setdnsservers "AirPort" 192.168.1.109
+fi
