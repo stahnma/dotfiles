6ed95e491cf3cd4cacc8c3abaae2c077289f04c0
stahnma <stahnma@websages.com>
2010-05-09 11:27:30 -0500

    Fixing my mac laptop resolver settings.

--- .bash.d/01_resolver.sh
+++ .bash.d/01_resolver.sh
@@ -3,10 +3,10 @@
 [ -f /usr/local/sbin/resolver.sh ] && sudo /usr/local/sbin/resolver.sh
 
 if [ `uname` == "Darwin" ] ; then
-   if (host x2.stahnkage.com 192.168.1.109 &> /dev/null) ; then 
-     networksetup -setsearchdomains AirPort stahnkage.com websages.com
-     networksetup -setdnsservers AirPort 192.168.1.109
-     networksetup -setsearchdomains Ethernet stahnkage.com websages.com
-     networksetup -setdnsservers Ethernet 192.168.1.109
-   fi 
+  if ( arp 192.168.1.1 | grep  0:16:b6:28:87:51  &> /dev/null ) ; then 
+   networksetup -setsearchdomains AirPort stahnkage.com websages.com
+   networksetup -setdnsservers AirPort 192.168.1.109
+   networksetup -setsearchdomains Ethernet stahnkage.com websages.com
+   networksetup -setdnsservers Ethernet 192.168.1.109
+  fi 
 fi
