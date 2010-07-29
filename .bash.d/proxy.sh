55631bb42bbcef7196b40a67dba95b66b09cef4e
stahnma <stahnma@websages.com>
2010-07-29 16:40:51 -0500

    Updating proxy.

--- .bash.d/proxy.sh
+++ .bash.d/proxy.sh
@@ -1,7 +1,7 @@
 
 if (host ncc01905.fpd.cat.com &> /dev/null ) ; then
-  export http_proxy=http://stahnma:${WEB_PIN}@webproxy.fpd.cat.com
+  export http_proxy=http://proxy.fpd.cat.com
   export no_proxy=cat.com
-  export HTTP_PROXY=http://stahnma:${WEB_PIN}@webproxy.fpd.cat.com
+  export HTTP_PROXY=http://proxy.fpd.cat.com
   export NO_PROXY=cat.com
 fi
