405759680690a6a7f0184b50ef3a72adb6de40b0
stahnma <stahnma@websages.com>
2010-08-02 08:59:08 -0500

    Fixed a spelling error.

--- .bash.d/keyboard_layout.sh
+++ .bash.d/keyboard_layout.sh
@@ -1,6 +1,6 @@
 
 if [ "$UNAME" = Linux ] ; then
-  if ( which xmodmpa &> /dev/null ) ; then 
+  if ( which xmodmap &> /dev/null ) ; then 
     xmodmap -e "remove lock = Caps_Lock"
     xmodmap -e "add control = Caps_Lock"
   fi 
