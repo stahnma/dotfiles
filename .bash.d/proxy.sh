142cb17a598c212da88254b8af432dcf6b9fec9f
stahnma <stahnma@websages.com>
2010-10-12 08:52:30 -0500

    Fixing proxy var for more compatiblity with python libs.

--- .bash.d/proxy.sh
+++ .bash.d/proxy.sh
@@ -1,7 +1,7 @@
 
 if (host ncc01905.fpd.cat.com &> /dev/null ) ; then
-  export http_proxy=http://proxy.fpd.cat.com
+  export http_proxy=http://proxy.fpd.cat.com:80
   export no_proxy=cat.com
-  export HTTP_PROXY=http://proxy.fpd.cat.com
+  export HTTP_PROXY=http://proxy.fpd.cat.com:80
   export NO_PROXY=cat.com
 fi
