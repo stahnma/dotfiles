f5f8f7a4b8b0ef1a0d6f185ae4e4160772f0a967
stahnma <stahnma@websages.com>
2011-04-19 09:49:03 -0700

    Changes for puppet labs

--- .bash.d/01_resolver.sh
+++ .bash.d/01_resolver.sh
@@ -3,6 +3,9 @@
 #[ -f /usr/local/sbin/resolver.sh ] && sudo /usr/local/sbin/resolver.sh
 
 
+hostname=`hostname -s`
+[ "$hostname" == "stahnma-box" ] && return
+
 if [ `uname` == "Darwin" ] ; then
   if ( arp 192.168.1.1 | grep  0:16:b6:28:87:51  &> /dev/null ) ; then 
     networksetup -setsearchdomains AirPort stahnkage.com websages.com
