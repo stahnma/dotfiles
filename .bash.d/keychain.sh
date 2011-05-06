7afcb99116d3d8b8660e3fc097387e4a0c69e6c7
stahnma <stahnma@websages.com>
2011-05-06 13:54:39 -0700

    Making keychain file work with id_rsa and id_dsa.

--- .bash.d/keychain.sh
+++ .bash.d/keychain.sh
@@ -1,12 +1,16 @@
 
-is_workstation
-if [ $? = 0 ] ; then 
-   # ennumerate all the -sh options
-   local chains=`ls $HOME/.keychain/*-sh`
-   for file in $chains
-   do
-      . $file
-   done
+# ennumerate all the -sh options
+chains=`ls $HOME/.keychain/*-sh`
+for file in $chains
+do
+    . $file
+done
+
+if [ -f "$HOME/.ssh/id_dsa" ] ; then 
    [ -z "$chains" ] && keychain $HOME/.ssh/id_dsa 
+fi
+
+if [ -f "$HOME/.ssh/id_rsa" ] ; then 
+   [ -z "$chains" ] && keychain $HOME/.ssh/id_rsa 
 fi
 
