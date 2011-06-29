d98a0bbdb66ad491cd3d577c2bbdce489eac27d5
Michael Stahnke <stahnma@puppetlabs.com>
2011-06-28 22:14:11 -0700

    Spacing error in xmodmap command.
    Signed-off-by: Michael Stahnke <stahnma@puppetlabs.com>

--- .bash.d/keyboard_layout.sh
+++ .bash.d/keyboard_layout.sh
@@ -1,6 +1,6 @@
 
 if [ "$UNAME" = Linux ] ; then
-  if ( which xmodmap &> /dev/null ) ; then 
+  if (which xmodmap &> /dev/null) ; then 
     xmodmap -e "remove lock = Caps_Lock" &> /dev/null
     xmodmap -e "add control = Caps_Lock" &> /dev/null
   fi 
