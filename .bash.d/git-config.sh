a9db255dfd8faf5a15cdd7e7b9e3a759e94e883b
Michael Stahnke <stahnma@puppetlabs.com>
2011-07-08 09:14:40 -0700

    Still fiddling with aliases

--- .bash.d/git-config.sh
+++ .bash.d/git-config.sh
@@ -8,8 +8,8 @@ git config --global color.branch "auto"
 git config --global color.ui "auto"
 git config --global github.user "stahnma"
 git config --global github.token "$GITHUB_TOKEN"
+git config --global user.signingkey "4BD6EC30"
 
 git config --global alias.ci commit
 git config --global alias.co checkout
 git config --global alias.br branch
-git config --global alias.tagg  'tag -s -u 4BD6EC30'
