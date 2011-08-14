b9df09fcd85ea43fdc301270c0d5581c38235174
Michael Stahnke <stahnma@puppetlabs.com>
2011-08-13 17:24:58 -0700

    Switching to simple variable for term reset

--- .bash.d/bash_prompts.sh
+++ .bash.d/bash_prompts.sh
@@ -11,7 +11,7 @@ normal_prompt()
 
 color_prompt()
 {
-    PS1="${ePL}\u@\h ${eBL} "'$PWD'"${GY}"'$(parse_git_branch)'"${eBL}> \[\e[0m\]"
+    PS1="${ePL}\u@\h ${eBL} "'$PWD'"${GY}"'$(parse_git_branch)'"${eBL}> ${NRML}"
 }
 
 # Prompt Goodness
