08d0c041b6fbfde9eaa6f051fb774ac560819298
Michael Stahnke <stahnma@puppetlabs.com>
2011-09-25 11:52:35 -0700

    Don't attempt to run networksetup

--- .bash.d/01_resolver.sh
+++ .bash.d/01_resolver.sh
@@ -6,16 +6,16 @@
 hostname=`hostname -s`
 [ "$hostname" == "stahnma-box" ] && return
 
-if [ `uname` == "Darwin" ] ; then
-  if ( arp 192.168.1.1 | grep  0:16:b6:28:87:51  &> /dev/null ) ; then 
-    networksetup -setsearchdomains AirPort stahnkage.com websages.com
-    networksetup -setdnsservers AirPort 192.168.1.109
-    networksetup -setsearchdomains Ethernet stahnkage.com websages.com
-    networksetup -setdnsservers Ethernet 192.168.1.109
-  else
-    networksetup -setsearchdomains AirPort websages.com
-    networksetup -setsearchdomains Ethernet websages.com
-    networksetup -setdnsservers AirPort "Empty"
-    networksetup -setdnsservers Ethernet "Empty"
-  fi 
-fi
+#if [ `uname` == "Darwin" ] ; then
+#  if ( arp 192.168.1.1 | grep  0:16:b6:28:87:51  &> /dev/null ) ; then 
+#    networksetup -setsearchdomains AirPort stahnkage.com websages.com
+#    networksetup -setdnsservers AirPort 192.168.1.109
+#    networksetup -setsearchdomains Ethernet stahnkage.com websages.com
+#    networksetup -setdnsservers Ethernet 192.168.1.109
+#  else
+#    networksetup -setsearchdomains AirPort websages.com
+#    networksetup -setsearchdomains Ethernet websages.com
+#    networksetup -setdnsservers AirPort "Empty"
+#    networksetup -setdnsservers Ethernet "Empty"
+#  fi 
+#fi
