c631e0d9e5141811c6221874a92b4cf96808428b
Michael Stahnke <stahnma@puppetlabs.com>
2011-09-25 12:05:10 -0700

    Update path on macos

--- .bash.d/01_path.sh
+++ .bash.d/01_path.sh
@@ -1,18 +1,19 @@
 # Figure out Path
 # Basically, I like having a lot in my path, so I check the system path and
 #  then apply what I like on top of it.
+uname=`uname`
 args="/usr/local/bin:/usr/local/sbin:$HOME/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/X11R6/bin:$HOME/.gem/ruby/1.8/bin"
 aix_args="/opt/freeware/bin:/etc"
 solaris_args="/usr/xpg4/bin"
 hp_args=""
 linux_args="/usr/kerberos/bin"
-mac_args=""
+mac_args="/opt/local/bin"
 
 PATH="$args"
 [ "$uname" = "Linux" ] && PATH=$linux_args:$PATH
 [ "$uname" = "SunOS" ] && PATH=$solaris_args:$PATH
 [ "$uname" = "AIX" ] && PATH=$aix_args:$PATH
-[ "$uname" = "Darwin" ] && PATH=$mac_args:$PATH:/opt/local/bin
+[ "$uname" = "Darwin" ] && PATH=$mac_args:$PATH
 [ "$uname" = "HPUX" ] && PATH=$hp_args:$PATH
 
 export PATH
