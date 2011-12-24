3492274ef93f9c29c3fb33375ba8fc1809b28577
Michael Stahnke <stahnma@puppetlabs.com>
2011-12-23 19:30:14 -0500

    Updates to keychain

--- .bash.d/keychain.sh
+++ .bash.d/keychain.sh
@@ -1,16 +1,18 @@
-chains=`ls $HOME/.keychain/$(hostname)-sh`
-for file in $chains
-do
-    . $file
-done
+if ls $HOME/.keychain/$(hostname)-sh &> /dev/null ; then
+  chains=`ls $HOME/.keychain/$(hostname)-sh`
+  for file in $chains
+  do
+      . $file
+  done
 
-if which keychian &> /dev/null ; then
-  if [ -f "$HOME/.ssh/id_dsa" ] ; then
-    [ -z "$chains" ] && keychain $HOME/.ssh/id_dsa
-  fi
+  if which keychian &> /dev/null ; then
+    if [ -f "$HOME/.ssh/id_dsa" ] ; then
+      [ -z "$chains" ] && keychain $HOME/.ssh/id_dsa
+    fi
 
-  if [ -f "$HOME/.ssh/id_rsa" ] ; then
-    [ -z "$chains" ] && keychain $HOME/.ssh/id_rsa
+    if [ -f "$HOME/.ssh/id_rsa" ] ; then
+      [ -z "$chains" ] && keychain $HOME/.ssh/id_rsa
+    fi
   fi
 fi
 
