f4469c9f3e7fdd32b9b0e6549cb8102c05d7ba11
Michael Stahnke <stahnma@puppetlabs.com>
2011-12-23 19:28:12 -0500

    Updates to keychain

--- .bash.d/keychain.sh
+++ .bash.d/keychain.sh
@@ -1,19 +1,17 @@
-
-# ennumerate all the -sh options
-ls $HOME/.keychain*-sh &> /dev/null  || return 0
-
 chains=`ls $HOME/.keychain/$(hostname)-sh`
 for file in $chains
 do
     . $file
 done
 
-if [ -f "$HOME/.ssh/id_dsa" ] ; then
-   [ -z "$chains" ] && keychain $HOME/.ssh/id_dsa
-fi
+if which keychian &> /dev/null ; then
+  if [ -f "$HOME/.ssh/id_dsa" ] ; then
+    [ -z "$chains" ] && keychain $HOME/.ssh/id_dsa
+  fi
 
-if [ -f "$HOME/.ssh/id_rsa" ] ; then
-   [ -z "$chains" ] && keychain $HOME/.ssh/id_rsa
+  if [ -f "$HOME/.ssh/id_rsa" ] ; then
+    [ -z "$chains" ] && keychain $HOME/.ssh/id_rsa
+  fi
 fi
 
 #GPG Agent
