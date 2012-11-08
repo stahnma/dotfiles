569a7be5e05866759b2e25fbc593fcfd87b6b1e0
Michael Stahnke <stahnma@puppetlabs.com>
2012-11-07 20:37:44 -0800

    Remove no longer used files

--- .bash.d/synergy.sh
+++
@@ -1,8 +0,0 @@
-
-return 0
-if [ -n "$WORKSTATION" ] ; then
-    if [ "$HOSTNAME" = "$WORKSTATION" ] ; then
-      ps -ef | grep [s]ynergys &> /dev/null || /usr/bin/synergys --config $HOME/.synergy.conf &> /dev/null
-    fi
-fi
-
